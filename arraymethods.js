15f74125471a0d2c6aeade3406ad7212e1785af8
Zacharae <zacharyxae@gmail.com>
2016-09-08 18:46:04 -0400

    array

---
+++ arraymethods.js
@@ -0,0 +1,21 @@
+var line = [1,2,3,4];
+
+for( var i=0; i < line.length; i++){
+	console.log(line[i]);
+}
+
+line.push(3);
+line.push(2);
+line.push(1);
+
+console.log(line);
+
+
+// function ittt(str){
+//     var stringContainer = [];
+//     for (var i = 0; i < str.length; i++) {
+//         stringContainer[i] = [i];
+//     }
+//     return stringContainer;
+// }
+// console.log(ittt)
